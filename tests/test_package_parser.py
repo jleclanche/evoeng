cb2c0ac2865bbc7955716aca3b4a07fb15b91fcb
Jerome Leclanche <jerome@leclan.ch>
2018-10-16 14:39:20 +0300

    Add regression tests for objects and expected structures

--- tests/test_package_parser.py
+++ tests/test_package_parser.py
@@ -9,7 +9,6 @@ with open(os.path.join(os.path.dirname(__file__), "packages.json"), "r") as f:
 	packages = json.load(f)
 
 
-@pytest.mark.slow
 @pytest.mark.parametrize(
 	"package_text",
 	packages,
@@ -17,3 +16,29 @@ with open(os.path.join(os.path.dirname(__file__), "packages.json"), "r") as f:
 )
 def test_packages(package_text):
 	assert package_parser.loads(package_text)
+
+
+PACKAGES = {
+	"A=B": {"A": "B"},
+	"A=1": {"A": 1},
+	"A=1.0": {"A": 1.0},
+	"A={}": {"A": []},
+	"A={\nA=1\n}": {"A": {"A": 1}},
+	"A={1,2,3}": {"A": [1, 2, 3]},
+	"A={\nRawString1,RawString2\n}": {"A": ["RawString1", "RawString2"]},
+	"A={\nRawString1,RawString2,\n}": {"A": ["RawString1", "RawString2"]},
+	"A=1x1": {"A": "1x1"},
+	"A=1.8000001e+05": {"A": 1.8000001e+05},
+	"A=-9.2029601e-05": {"A": -9.2029601e-05},
+	"A=88c1934b-3e5e-4f63-a599-1670f585aee2": {"A": "88c1934b-3e5e-4f63-a599-1670f585aee2"},
+	'A={\nB=""\n}': {"A": {"B": ""}},
+	'A={\nB="https://example.com/?a=b"\n}': {"A": {"B": "https://example.com/?a=b"}},
+}
+
+@pytest.mark.parametrize(
+	("package_text,expected_value"),
+	PACKAGES.items(),
+	ids=lambda s: s if isinstance(s, str) else repr(s)
+)
+def test_package_correct_structure(package_text, expected_value):
+	assert package_parser.loads("\n" + package_text + "\n") == expected_value
